ba764511601a525597677139900c71f7f4778560
andrekisi <andre@kisi.io>
2020-07-20 19:15:43 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    const arr = [1, 2, 3];
+    // sort is not defined:
+    const sorted = sort(arr);
+    return { sorted };
+  }
+
+  /*  fetch() {
+    const arr = [1, 2, 3];
+    // sort is not defined:
+    this.sorted = sort(arr);
+  }*/
+};
+</script>
